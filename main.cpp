76ccf4bd8816f4c5d7b753bbbf842e35cb57ba80
Krasnoffsky <senior00krasnoff@gmail.com>
2019-07-01 21:07:22 +0700

    complete edition

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <QCoreApplication>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <ctime>
 #include <vector>
 using namespace std;
+
 string wordState = "";	//For the current word state
 string defaultWords[12] = {"Programming", "Language", "C Plus Plus", "Recursion", "UML", "Class Inheritance", 
 						"Asymptotic Notation", "Space Complexity", "Exchange Sort", "Quick Sort", "Templates", "Operator Overloading"};
@@ -105,6 +107,34 @@ void displayCurFigure(int max, int guessNum){
 	}
 }
 
+//Set words list:
+void setWordsList(vector<string> & list, string file){
+	string line;
+	ifstream readline;
+	readline.open(file);
+	while(true){
+		if(getline(readline, line)){
+			list.push_back(line);
+		}
+		else{
+			SIZE = list.size();
+			readline.close();
+			break;
+		}
+	}
+
+	int k;	//Used as an index
+	//Check for any characters that are not letters:
+	for(int i = 0; i < list.size(); i++){
+		k = list[i].find_first_of("1234567890-=`~!@#$%^&*)(}{][|\\:\";\'><.,?/_+");
+		while(k != string::npos){
+			list[i].erase(list[i].begin() + k);
+			k = list[i].find_first_of("1234567890-=`~!@#$%^&*)(}{][|\\:\";\'><.,?/_+");
+		}
+	}
+
+}
+
 //Create the starting wordState
 void createWordState(string word){
 	for(int i = 0; i < word.length(); i++){
@@ -117,12 +147,43 @@ void createWordState(string word){
 	}
 }
 
+//Check if word is complete and correct
 bool finishedWord(string word){
 	return (wordState == word);
 }
+
+//Creating a temporary copy of the original word
+string createTemp(string origWord){
+	string temp = origWord;
+	
+	for(int i = 0; i < temp.length(); i++){
+		temp[i] = tolower(temp[i]);
+	}
+
+	return temp;
+}
+
+//Display current state of 'wordState'
+void displayWordState(){
+	int wordCount = 0;
+	for(int i = 0; i < wordState.length(); i++){
+		if(wordState[i] == ' '){
+			++wordCount;
+			if(wordCount > 4){
+				cout << endl;
+				wordCount = 0;
+			}
+		}
+		cout << wordState[i] << " ";
+	}
+	cout << endl;
+}
+
+//Check if letter exists in the original word
 bool letterExists(char letter, string tempWord){
 	return (tempWord.find_first_of(letter) != string::npos);
 }
+
 //IF THE LETTER EXISTS, add it to 'wordState'
 void changeWordState(char letter, string wordTemp, string word){
 	for(int i = 0; i < wordState.length(); i++){
@@ -155,53 +216,6 @@ void addLettersUsed(char letter){
 }
 
 
-//Display current state of 'wordState'
-void displayWordState(){
-	int wordCount = 0;
-	for(int i = 0; i < wordState.length(); i++){
-		if(wordState[i] == ' '){
-			++wordCount;
-			if(wordCount > 4){
-				cout << endl;
-				wordCount = 0;
-			}
-		}
-		cout << wordState[i] << " ";
-	}
-	cout << endl;
-}
-
-void addWordsUsed(string word){
-	wordsUsed.push_back(word);
-}
-
-//Set words list:
-void setWordsList(vector<string> & list, string file){
-	string line;
-	ifstream readline;
-	readline.open(file);
-	while(true){
-		if(getline(readline, line)){
-			list.push_back(line);
-		}
-		else{
-			SIZE = list.size();
-			readline.close();
-			break;
-		}
-	}
-
-	int k;	//Used as an index
-	//Check for any characters that are not letters:
-	for(int i = 0; i < list.size(); i++){
-		k = list[i].find_first_of("1234567890-=`~!@#$%^&*)(}{][|\\:\";\'><.,?/_+");
-		while(k != string::npos){
-			list[i].erase(list[i].begin() + k);
-			k = list[i].find_first_of("1234567890-=`~!@#$%^&*)(}{][|\\:\";\'><.,?/_+");
-		}
-	}
-
-}
 
 //Display the letters the user already input
 void displayLettersUsed(){
@@ -219,6 +233,22 @@ void displayLettersUsed(){
 	}
 }
 
+//HARD MODE ONLY:
+//Check if any of the letters previously entered are in the new word
+void checkLettersUsed_HARD(string tempWord, string origWord){
+	char tempLetter;
+	for(int i = 0; i < lettersUsed.size(); i++){
+		tempLetter = tolower(lettersUsed[i]);
+		if(letterExists(tempLetter, tempWord)){
+			changeWordState(tempLetter, tempWord, origWord);
+		}
+	}
+}
+
+void addWordsUsed(string word){
+	wordsUsed.push_back(word);
+}
+
 void displayWordsUsed(){
 	int j;
 	for(int i = 0; i < wordsUsed.size(); i++){
@@ -227,7 +257,6 @@ void displayWordsUsed(){
 	}
 	cout << endl;
 }
-
 
 /**********EASY/NORMAL MODE**********/
 void startGame_EASY_NORMAL(string word, int MAX_GUESSES){
@@ -374,9 +403,9 @@ void startGame_HARD(vector<string> list, int MAX_GUESSES){
 }
 /********************************************/
 
-
 /*********MAIN*********/
-int main(){
+int main(int argc, char *argv[]){
+	QCoreApplication a(argc, argv);
 	string word;
 	string fileName;
 	vector<string> wordsList;
@@ -498,6 +527,5 @@ int main(){
 		}
 	}
 
-	return 0;
+	return a.exec();
 }
-
